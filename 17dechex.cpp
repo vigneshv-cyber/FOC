7e9cd9627db32a3a6aa4e97b8e1d9b786d40b91c
vignesh_cyber <113492992+vigneshv-cyber@users.noreply.github.com>
2023-01-10 13:28:34 +0530

    Add files via upload

---
+++ 17dechex.cpp
@@ -0,0 +1,11 @@
+//decimal to Hexadecimal
+#include<stdio.h>
+int main()
+{
+	int d;
+	printf("Enter the decimal value --> ");
+	scanf("%d",&d);
+	printf("\n\tDecimal value --> %d",d);
+	printf("\n\tHexadecimal value --> %x",d);
+	
+}
